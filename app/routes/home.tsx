fb3d6dc4cd7ce644b76a1760e1c2dd3690331e3b
Sakshi Hedke <sakshihedke6@gmail.com>
2025-07-22 08:10:04 +0530

    Cloud storage, PDF_Image conversion

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -24,7 +24,6 @@ export default function Home() {
   
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
-    {/* {window.puter.ai.chat()} */}
     <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
